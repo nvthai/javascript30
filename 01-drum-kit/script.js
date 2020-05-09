29c7ac3fc42f363d829c658a6fdbcf3a52c71de0
Thai Nguyen <nvthai.fr@gmail.com>
2020-05-09 16:47:39 +0700

    day1: drum kit

---
+++ 01-drum-kit/script.js
@@ -0,0 +1,22 @@
+const playSound = (event) => {
+  const audio = document.querySelector(`audio[data-key="${event.keyCode}"]`);
+  const key = document.querySelector(`div[data-key="${event.keyCode}"]`);
+
+  if (!key || !audio) return;
+
+  key.classList.add("playing");
+  audio.currentTime = 0;
+  audio.play();
+};
+
+const removeTransition = (event) => {
+  if (event.propertyName !== "transform") return;
+  event.target.classList.remove("playing");
+};
+
+const keys = Array.from(document.querySelectorAll(".key"));
+keys.forEach((key) => {
+  key.addEventListener("transitionend", removeTransition);
+});
+
+window.addEventListener("keydown", playSound);
